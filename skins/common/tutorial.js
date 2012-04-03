b16590715ab40391dea3629a20e8eb9c3e034dfb
Derrick Coetzee <dc@moonflare.com>
2012-04-03 03:32:21 -0700

    Move step names into 'namespace' for first lesson to make it easier to separate from steps for other lessons

--- skins/common/tutorial.js
+++ skins/common/tutorial.js
@@ -104,74 +104,78 @@ var updateOverlays = function() {
     arrowTarget = null;
 
     if (getStep() == 'start') {
+        setStep("first-edit.welcome");
+    }
+    
+    if (getStep() == "first-edit.welcome") {
         instructions.innerHTML = '<p>Welcome to <b>The Wikipedia Adventure</b>, a tutorial for new Wikipedia users. Instructions will appear in the lower-right.</p>';
         instructions.innerHTML += '<p>Would you like to review how to make an edit to an article?</p>';
-        instructions.innerHTML += '<p><a onclick="goToStep(\'main-page-click-article\');">Yes</a> <a onclick="goToStep(\'level-menu\');">No</a></p>';
+        instructions.innerHTML += '<p><a onclick="goToStep(\'first-edit.main-page-click-article\');">Yes</a> <a onclick="goToStep(\'level-menu\');">No</a></p>';
         centerElement(instructions);
-      } else if (getStep() == "main-page-click-article") {
+    } else if (getStep() == "first-edit.main-page-click-article") {
         instructions.innerHTML = '<p>Begin by clicking on the <b>George Tupou V</b> link to visit the article on George Tupou V, King of Tonga.</p>';
         arrowTarget = 'a[title="George Tupou V"]';
-        $(arrowTarget)[0].onclick = function(){setStep('find-error')};
-    } else if (getStep() == 'find-error') {
+        $(arrowTarget)[0].onclick = function(){setStep('first-edit.find-error')};
+    } else if (getStep() == 'first-edit.find-error') {
         instructions.innerHTML = '<p>You are now reading the article on George Tupou V. This article contains a small error in its first sentence. Try to find it and click on it.</p>';
         var content = $("div[class=mw-content-ltr]")[0];
-        content.innerHTML = content.innerHTML.replace(" deth ", ' <span id="deth" onclick="goToStep(\'show-error\')">deth</span> ');
-    } else if (getStep() == 'show-error') {
+        content.innerHTML = content.innerHTML.replace(" deth ", ' <span id="deth" onclick="goToStep(\'first-edit.show-error\')">deth</span> ');
+    } else if (getStep() == 'first-edit.show-error') {
         instructions.innerHTML = '<p>Nice spotting! The error is that "death" is misspelled as "deth". We\'re going to fix this.</p>';
-        instructions.innerHTML += '<p><a onclick="goToStep(\'click-edit-tab\');">Next</a></p>';
+        instructions.innerHTML += '<p><a onclick="goToStep(\'first-edit.click-edit-tab\');">Next</a></p>';
         $('#deth')[0].onclick = null;
         arrowTarget = '#deth';
-    } else if (getStep() == 'click-edit-tab') {
+    } else if (getStep() == 'first-edit.click-edit-tab') {
         instructions.innerHTML = '<p>Start by clicking on the <b>Edit</b> tab to edit the article.</p>';
         arrowTarget = '#ca-edit';
-        $(arrowTarget).find('a')[0].onclick = function(){setStep('editing');};
-    } else if (getStep() == 'editing') {
+        $(arrowTarget).find('a')[0].onclick = function(){setStep('first-edit.editing');};
+    } else if (getStep() == 'first-edit.editing') {
         instructions.innerHTML = '<p>You are now editing the article. Find the misspelled word <b>deth</b> and change it to "death", then click <b>Next</b> below.</p>';
-        instructions.innerHTML += '<p><a onclick="goToStep(\'done-editing\');">Next</a></p>';
+        instructions.innerHTML += '<p><a onclick="goToStep(\'first-edit.done-editing\');">Next</a></p>';
         desired_value = $('#wpTextbox1')[0].value.replace(' deth ', ' death ');
-    } else if (getStep() == 'done-editing') {
+    } else if (getStep() == 'first-edit.done-editing') {
         if ($('#wpTextbox1')[0].value == desired_value) {
             instructions.innerHTML = '<p>Good job! Next, click on the <b>Summary</b> field and enter a short explanation for your edit, such as "fixed spelling error". This is called an <i>edit summary</i>, and every edit should have one. Then click <b>Next</b> below.</p>';
-            instructions.innerHTML += '<p><a onclick="goToStep(\'done-summary\');">Next</a></p>';
+            instructions.innerHTML += '<p><a onclick="goToStep(\'first-edit.done-summary\');">Next</a></p>';
             arrowTarget = '#wpSummary';
         } else {
             instructions.innerHTML = '<p>You made the change incorrectly. Make sure you changed <b>deth</b> to "death" and <b>made no other changes</b>. Click <b>Next</b> below to try again.</p>';
-            instructions.innerHTML += '<p><a onclick="goToStep(\'done-editing\');">Next</a></p>';
+            instructions.innerHTML += '<p><a onclick="goToStep(\'first-edit.done-editing\');">Next</a></p>';
         }
-    } else if (getStep() == 'done-summary') {
+    } else if (getStep() == 'first-edit.done-summary') {
         if ($('#wpSummary')[0].value.length > 0) {
             instructions.innerHTML = '<p>Great! Finally, click on <b>Save page</b> to save your changes to the article.</p>';
             $('#editform')[0].onsubmit = function(){
                 // Don't really let them save - just redirect back to article, we'll fake the changes.
-                setStep('show-article-with-change');
+                setStep('first-edit.show-article-with-change');
                 window.location.href = wgArticlePath.replace('$1', 'George_Tupou_V');
                 return false;
             }
             arrowTarget = '#wpSave';
         } else {
             instructions.innerHTML = '<p>You did not enter a summary. Click on the <b>Summary</b> field indicated by the arrow and enter a short explanation for your edit, such as "fixed spelling error". Then click <b>Next</b> below.</p>';
-            instructions.innerHTML += '<p><a onclick="goToStep(\'done-summary\');">Next</a></p>';
+            instructions.innerHTML += '<p><a onclick="goToStep(\'first-edit.done-summary\');">Next</a></p>';
         }
-    } else if (getStep() == 'show-article-with-change') {
+    } else if (getStep() == 'first-edit.show-article-with-change') {
         instructions.innerHTML = '<p>Your changes to the article appear instantly to all readers. If your changes were incorrect, another user can always easily change it back. Click <b>Next</b> below for your <b>Real Wikipedia Bonus Task</b>.</p>';
-        instructions.innerHTML += '<p><a onclick="goToStep(\'real-wikipedia-bonus-task\');">Next</a></p>';
+        instructions.innerHTML += '<p><a onclick="goToStep(\'first-edit.real-wikipedia-bonus-task\');">Next</a></p>';
         var content = $("div[class=mw-content-ltr]")[0];
         content.innerHTML = content.innerHTML.replace(" deth ", ' <span id="death">death</span> ');
         arrowTarget = '#death';
-    } else if (getStep() == 'real-wikipedia-bonus-task') {
+    } else if (getStep() == 'first-edit.real-wikipedia-bonus-task') {
         instructions.innerHTML = '<p><b>Real Wikipedia Bonus Task</b></p>';
         instructions.innerHTML += '<p>You\'re now ready to make a real edit to Wikipedia. ';
         instructions.innerHTML += 'Visit <a href="http://en.wikipedia.org" onclick="$(this).attr(\'target\', \'_blank\');">en.wikipedia.org</a> and click on the <b>Random article</b> link (indicated). ';
         instructions.innerHTML += 'Look for a minor error in spelling, grammar, punctuation, or style and fix it. If you don\'t see one, click <b>Random article</b> again until you do. When done, click <b>Next</b> below.</p>';
-        instructions.innerHTML += '<p><a onclick="goToStep(\'lesson-complete\');">Next</a></p>';
+        instructions.innerHTML += '<p><a onclick="goToStep(\'first-edit.lesson-complete\');">Next</a></p>';
         arrowTarget = '#n-randompage';
-    } else if (getStep() == 'lesson-complete') {
+    } else if (getStep() == 'first-edit.lesson-complete') {
         instructions.innerHTML = '<p>Great job! You have completed the first level. Click <b>Level menu</b> below to choose another level.</p>';
         instructions.innerHTML += '<p><a onclick="goToStep(\'level-menu\');">Level menu</a></p>';
         centerElement(instructions);
     } else if (getStep() == 'level-menu') {
         instructions.innerHTML = '<p><b>Select a level</b></p>';
-        instructions.innerHTML += '<p><a href="' + wgArticlePath.replace('$1', 'Main_Page') + '" onclick="setStep(\'start\');">Making your first edit</a></p>';
+        instructions.innerHTML += '<p><a href="' + wgArticlePath.replace('$1', 'Main_Page') + '" onclick="setStep(\'first-edit.welcome\');">Making your first edit</a></p>';
         instructions.innerHTML += '<p><i>More levels to come!</i></p>';
         centerElement(instructions);
     } else {
